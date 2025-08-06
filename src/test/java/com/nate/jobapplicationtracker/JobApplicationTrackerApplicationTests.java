64cd3a68ebb8c4f35455f343240f8a03bc5fa35e
Nathan Pombi <pombinathan@gmail.com>
2025-08-06 16:57:02 +0200

    Initial project setup

---
+++ src/test/java/com/nate/jobapplicationtracker/JobApplicationTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nate.jobapplicationtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
